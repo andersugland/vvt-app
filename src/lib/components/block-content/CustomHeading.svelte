cde92daa059f760688502532b986930b51d565ab
Anders Ugland <ugland.anders@gmail.com>
2023-12-08 11:21:27 +0100

    Make font size dynamic on smaller screens

--- src/lib/components/block-content/CustomHeading.svelte
+++ src/lib/components/block-content/CustomHeading.svelte
@@ -8,19 +8,19 @@
 </script>
 
 {#if value?.style === 'h2'}
-	<h2 class="text-heading md:text-2xl">
+	<h2 class="text-2xl">
 		<slot />
 	</h2>
 {:else if value?.style === 'h3'}
-	<h3 class="text-heading md:text-xl">
+	<h3 class="text-xl">
 		<slot />
 	</h3>
 {:else if value?.style === 'h4'}
-	<h4 class="text-heading md:text-lg">
+	<h4 class="text-lg">
 		<slot />
 	</h4>
 {:else if value?.style === 'h4'}
-	<h5 class="text-heading md:text-md">
+	<h5 class="text-md">
 		<slot />
 	</h5>
 {/if}
